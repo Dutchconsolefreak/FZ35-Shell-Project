93b7f4483ae21843f4411236d37589494e5a8849
Dutchconsolefreak <leon382@gmail.com>
2026-04-25 01:25:17 +0200

    Initial commit

---
+++ src/cmds/softreset/softreset.c
@@ -0,0 +1,1 @@
+#include <kernel:types.h>#include <kernel:driver.h>#include "serialdebug.h"int main(int argc, char **argv){    SerialOpen();    SerialPrintf("Performing soft reset...\r\n");    SerialClose();    SoftReset();    return 0;  /* never reached */}
